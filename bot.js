e31f2384a17ba1dd99c959491060c62a6d74cafe
Chloe Chong <chloechong.us@gmail.com>
2018-09-17 16:24:25 -0400

    Added methods to class bot

--- bot.js
+++ bot.js
@@ -6,4 +6,25 @@ class Bot {
     this.claims = [];
     this.score = 0;
   }
+  updateLocation = (x, y) => {
+    this.location = [x, y];
+  };
+  isRegistered = () => {
+    return this.register;
+  };
+  toggleActive = () => {
+    this.active = !this.active;
+  };
+  updateScore = score => {
+    this.score = score;
+  };
+  addClaim = claim => {
+    if (this.claims.length > 3) return 'Exceeded maximum';
+    else this.claims.push(claim);
+  };
+  removeClaim = claim => {
+    this.claims = this.claims.filter(x => x !== claim);
+  };
 }
+
+module.exports = Bot;
