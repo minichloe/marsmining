1eb041157c08a6dd54e2ae91274981953c9eba5f
Chloe Chong <chloechong.us@gmail.com>
2018-09-17 15:20:57 -0400

    Added callsign to register

--- register.js
+++ register.js
@@ -2,7 +2,10 @@ const axios = require('axios');
 
 const register = async () => {
   try {
-    const data = await axios.post('/register');
+    const callsign = {
+      callsign: 'SierraEcho',
+    };
+    const { data } = await axios.post('/register', callsign);
     console.log(data);
   } catch (err) {
     console.error(err);
